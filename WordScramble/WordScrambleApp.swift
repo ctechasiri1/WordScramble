c97311748d463056363b00a4e222effd8c1e3f39
Chiraphat <ctechasiri@berkeley.edu>
2024-10-08 18:51:58 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Chiraphat Techasiri on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
